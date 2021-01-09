acc6482b043703e714a618f44db95e56175b82e1
jessicayangg01 <59785485+jessicayangg01@users.noreply.github.com>
2021-01-08 19:00:05 -0500

    image
    just added an image

--- roommates/src/App.js
+++ roommates/src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
+import roomImage from './img/sven-brandsma-GmRiN7tVW1w-unsplash.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <img src={roomImage}  alt="image of decorated room" style={{width: "100%", height: "auto"}}/>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
